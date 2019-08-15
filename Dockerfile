1c958e9172e6dd3470a5936a49528d568784a16e
Radoslaw <maniaczek7@wp.pl>
2019-08-15 22:51:54 +0200

    update docker file to build on docker hub

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM gradle:4.10-jdk8 as builder
 COPY --chown=gradle:gradle . /home/gradle/src
 WORKDIR /home/gradle/src
 RUN gradle clean build
+RUN cd build/libs
+RUN ls -l -a
 
 FROM openjdk:10-jre-slim
 COPY --from=builder /home/gradle/src/build/libs/app-0.0.1.jar /app/app.jar
--- build.sh
+++ build.sh
@@ -2,11 +2,9 @@
 # remember to click maven/gradle install to create JAR file!
 ./gradlew clean build
 eval $("C:\Program Files\Docker Toolbox\docker-machine.exe" env dev)
-eval $("C:\Program Files\Docker Toolbox\docker-machine.exe" env dev)
-docker build -t rmichniewskihome/application:zuul .
-docker push rmichniewskihome/application:zuul
-ssh ec2 docker pull rmichniewskihome/application:zuul
-ssh ec2 docker rm -f zuul
-#ssh ec2 docker run -d --name=zuul -p 8762:8762 --rm rmichniewskihome/application:zuul
-ssh ec2 docker run -d --name=zuul -p 80:80 --rm rmichniewskihome/application:zuul
+docker build -t rmichniewskihome/back-end_zuul:latest .
+docker push rmichniewskihome/back-end_zuul:latest
+ssh ec2 docker pull rmichniewskihome/back-end_zuul:latest
+ssh ec2 docker rm -f back-end_zuul:latest
+ssh ec2 docker run -d --name=zuul -p 80:80 --rm rmichniewskihome/back-end_zuul:latest
 ssh ec2 docker ps
